9e821f69bd9edbd126416cb499084a5d9109d4ca
Jhonatan <jhonatan.dev90@gmail.com>
2023-11-05 18:32:38 -0300

    Atualização do HTM e  CSS, inserção do JavaScript

---
+++ script.js
@@ -0,0 +1,50 @@
+// "Banco de dados"
+let participantes = []
+
+// Elementos Da Tela
+// Funções de Entrada (Capturar valores do Input)
+const inputParticipante = document.getElementById('participante')
+function obterParticipante() {
+  return inputParticipante.value.trim()
+}
+
+// Funções de Saída (Colocar valores dentro das tags HTML)
+const ulParticipantes = document.getElementById('participantes')
+function mostrarParticipantes(participantes) {
+  ulParticipantes.innerHTML = ''
+
+  for (let participante of participantes) {
+    // Criando tag do participante
+    let liParticipante = document.createElement('li')
+    liParticipante.textContent = participante
+    liParticipante.classList.add('participante')
+
+    ulParticipantes.appendChild(liParticipante)
+  }
+}
+
+let pSorteio = document.getElementById('participante-sorteado')
+function mostrarSorteado(participanteSorteado) {
+  pSorteio.innerText = `Sorteado: ${participanteSorteado}`
+}
+
+// Funções de Handle (Funções para lidar com os eventos DOM)
+// Essas funções, chamarão as funções de entrada e saida.
+function handleAdicionarParticipante() {
+  let participante = obterParticipante()
+  participantes.push(participante)
+  mostrarParticipantes(participantes)
+}
+
+function handleSortear() {
+  let posicaoSorteada = Math.floor(Math.random() * participantes.length)
+  let participanteSorteado = participantes[posicaoSorteada]
+  mostrarSorteado(participanteSorteado)
+}
+
+// Event Listers
+const botaoAdicionarParticipante = document.querySelector('#botao-adicionar-participante') 
+const botaoSortear = document.querySelector('#botao-sortear')
+
+botaoAdicionarParticipante.addEventListener('click', handleAdicionarParticipante)
+botaoSortear.addEventListener('click', handleSortear)
